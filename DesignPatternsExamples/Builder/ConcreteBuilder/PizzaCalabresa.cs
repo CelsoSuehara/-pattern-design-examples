e6213f6b17dd5b7e24239e8fc903c46788c157b8
Celso <celso.suehara@hotmail.com>
2024-08-10 19:49:23 -0300

    exemplo do pattern builder

---
+++ DesignPatternsExamples/Builder/ConcreteBuilder/PizzaCalabresa.cs
@@ -0,0 +1,19 @@
+﻿using Builder.Builder;
+using Builder.Product;
+
+namespace Builder.ConcreteBuilder;
+
+public sealed class PizzaCalabresa : PizzaBuilder
+{
+    public override void PreparaPizza()
+    {
+        pizza.TipoMassa = TipoMassa.Grossa;
+        pizza.TipoBorda = TipoBorda.Normal;
+        pizza.Tamanho = Tamanho.Grande;
+    }
+
+    public override void IncluiIngredientes()
+    {
+        pizza.Ingredientes = new List<string> { "Calabresa fatiada", "Molho de tomate" };
+    }
+}
