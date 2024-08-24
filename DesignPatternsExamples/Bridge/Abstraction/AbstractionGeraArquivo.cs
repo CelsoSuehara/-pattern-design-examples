172a27a82edf3862c43ae02157c080f447a2ea8f
Celso <celso.suehara@hotmail.com>
2024-08-24 19:29:58 -0300

    exemplo do padrão bridge

---
+++ DesignPatternsExamples/Bridge/Abstraction/AbstractionGeraArquivo.cs
@@ -0,0 +1,19 @@
+﻿using Bridge.Domain;
+using Bridge.Implementor;
+
+namespace Bridge.Abstraction;
+
+public abstract class AbstractionGeraArquivo
+{
+    protected IGeraArquivo _geraArquivo;
+
+    protected AbstractionGeraArquivo(IGeraArquivo geraArquivo)
+    {
+        _geraArquivo = geraArquivo;
+    }
+
+    public virtual void GravaArquivo(Funcionario funcionario)
+    {
+        _geraArquivo.GravaArquivo(funcionario);
+    }
+}
---
+++ DesignPatternsExamples/Bridge/RefinedAbstraction/CalculaSalario.cs
@@ -0,0 +1,21 @@
+﻿using Bridge.Abstraction;
+using Bridge.Domain;
+using Bridge.Implementor;
+
+namespace Bridge.RefinedAbstraction;
+
+public class CalculaSalario : AbstractionGeraArquivo
+{
+    public CalculaSalario(IGeraArquivo geraArquivo) : base(geraArquivo)
+    {
+    }
+
+    public void ProcessaSalarioFuncionario(Funcionario funcionario)
+    {
+        funcionario.SalarioTotal = funcionario.SalarioBase + funcionario.Incentivo;
+
+        Console.WriteLine($"Valor do salário total para o funcionário: {funcionario.Nome} > R$ {funcionario.SalarioTotal}");
+
+        _geraArquivo.GravaArquivo(funcionario);
+    }
+}
