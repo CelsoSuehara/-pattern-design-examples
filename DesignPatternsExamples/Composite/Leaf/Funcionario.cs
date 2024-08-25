1f3c7ea4d40bdd8bd63072d6eac186fc10e67b98
Celso <celso.suehara@hotmail.com>
2024-08-25 15:33:59 -0300

    exemplo do padrão composite

---
+++ DesignPatternsExamples/Composite/Leaf/Funcionario.cs
@@ -0,0 +1,16 @@
+﻿using Composite.Component;
+
+namespace Composite.Leaf;
+
+public class Funcionario : HoraTrabalhada
+{
+    public int Id { get; set; }
+
+    public int Horas { get; set; }
+
+    public override int GetHoraTrabalhada()
+    {
+        Console.WriteLine($"O funcionário {Id}-{Nome} registrou {Horas} horas trabalhadas.");
+        return Horas;
+    }
+}
